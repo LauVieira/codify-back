91ce89553f367273313f3efecc14979a1d7e146c
Gabriel Milhomem <gabriell.mil@gmail.com>
2021-02-02 15:18:24 -0300

    Att Libs, script, pastes, and config.js (production)

--- config/config.js
+++ config/config.js
@@ -1,11 +1,15 @@
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
   },
+
   test: {
     url: process.env.DATABASE_URL,
   },
+
+  production: {
+    url: process.env.DATABASE_URL
+  }
 };
--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
+require('express-async-errors');
 require('./utils/loadRelationships');
+
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
+
+app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
 
-
+app.use((error, res, req, next) => {
+    console.error(error);
+    res.status(500).send({message: 'Internet server error'});
+});
 
 module.exports = app;
