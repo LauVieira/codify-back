1e532bb49609756d2b501c38f0430a01afa2b974
samarafsantos <samarasantos@poli.ufrj.br>
2021-02-04 22:55:42 -0300

    topics migration created

---
+++ migrations/20210205015156-create-topics-table.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('topics', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        unique: true
+      },
+      courseId: {
+        type: Sequelize.INTEGER,
+        references: {
+					model: 'courses',
+					key: 'id'
+				}
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('topics');
+  }
+};
