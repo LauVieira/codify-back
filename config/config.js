6289f7c38bb059182ae8fd3efa8face0c07d2ace
Laura Vieira <lauramvieira.aus@gmail.com>
2021-02-08 15:31:56 -0300

    fix: deploy configuration ssl rejection

--- config/config.js
+++ config/config.js
@@ -10,6 +10,7 @@ module.exports = {
   },
 
   production: {
-    url: process.env.DATABASE_URL
+    url: process.env.DATABASE_URL,
+    dialectOptions: { ssl: { rejectUnauthorized: false } }
   }
 };
