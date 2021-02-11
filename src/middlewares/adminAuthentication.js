a8991523e2ada8a4e960448831721d7da7b867aa
Laura Vieira <lauramvieira.aus@gmail.com>
2021-02-11 15:14:20 -0300

    feat: adminAuthentication

--- src/middlewares/adminAuthentication.js
+++ src/middlewares/adminAuthentication.js
@@ -1,12 +1,16 @@
 const jwt = require('jsonwebtoken');
 const { UnauthorizedError } = require('../errors');
+const AdminController = require('../controllers/AdminController');
 
 async function adminAuthentication (req, res, next) {
   const { token } = req.cookies;
   if (!token) throw new UnauthorizedError('Token não encontrado');
 
-  jwt.verify(token, process.env.SECRET, (err, decoded) => {
+  jwt.verify(token, process.env.SECRET, async (err, decoded) => {
     if (err) throw new UnauthorizedError('Token inválido');
+
+    await AdminController.validateUserAndPassword(decoded.admin);
+
     req.admin = decoded.admin;
     next();
   });
