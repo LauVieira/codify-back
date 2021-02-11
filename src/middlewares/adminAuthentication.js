ef4fa1d348edeb6557ddb5d25d206d5d3d1d5144
Laura Vieira <lauramvieira.aus@gmail.com>
2021-02-11 13:20:52 -0300

    feat: starting adminAuthentication

--- src/middlewares/adminAuthentication.js
+++ src/middlewares/adminAuthentication.js
@@ -1,15 +1,15 @@
 const jwt = require('jsonwebtoken');
 const { UnauthorizedError } = require('../errors');
 
-async function userAuthentication (req, res, next) {
+async function adminAuthentication (req, res, next) {
   const { token } = req.cookies;
   if (!token) throw new UnauthorizedError('Token não encontrado');
 
   jwt.verify(token, process.env.SECRET, (err, decoded) => {
     if (err) throw new UnauthorizedError('Token inválido');
-    req.user = decoded.selectedUser;
+    req.admin = decoded.admin;
     next();
   });
 }
 
-module.exports = userAuthentication;
+module.exports = adminAuthentication;
