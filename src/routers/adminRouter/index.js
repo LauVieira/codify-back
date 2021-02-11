879382083914daa829644eec7aaad2fdb44c5f56
Laura Vieira <lauramvieira.aus@gmail.com>
2021-02-11 20:41:33 -0300

    starting manual admin router

--- src/routers/adminRouter/index.js
+++ src/routers/adminRouter/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const adminUsersRouter = require('./adminUsersRouter');
+const adminAuthentication = require('../../middlewares/adminAuthentication');
 
 router.use('/users', adminUsersRouter);
 
