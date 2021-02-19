6fdac70fe1993090312b49514054485f71c4a235
Gabriel Milhomem <gabriell.mil@gmail.com>
2021-02-19 00:39:33 -0300

    unitary test on CoursesController.test.js

--- tests/controllers/CoursesController.test.js
+++ tests/controllers/CoursesController.test.js
@@ -1,11 +1,15 @@
 /* global jest, describe, it, expect  */
 const CoursesController = require('../../src/controllers/CoursesController');
 const Course = require('../../src/models/Course');
+const Topic = require('../../src/models/Topic');
+const Chapter = require('../../src/models/Chapter');
+const Activity = require('../../src/models/Activity');
+const Err = require('../../src/errors');
 
 jest.mock('sequelize');
 
 describe('getSuggestions', () => {
-  it('should return an array', async () => {
+  it('should return an array', () => {
     const spy = jest.spyOn(Course, 'findAll');
     CoursesController.getSuggestions(10);
 
@@ -17,12 +21,100 @@ describe('getSuggestions', () => {
 });
 
 describe('getCourse', () => {
-  it('should return a course', async () => {
+  it('should return a course', () => {
     const id = 1;
     const spy = jest.spyOn(Course, 'findByPk');
     CoursesController.getCourse(id);
 
     expect(spy).toHaveBeenCalled();
     expect(spy).toHaveBeenCalledWith(id);
+  });
+});
+
+describe('getTopic', () => {
+  it('should return a topic', () => {
+    const id = 1;
+    const spy = jest.spyOn(Topic, 'findByPk');
+    CoursesController.getTopic(id);
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith(id, expect.any(Object));
+  });
+
+  it('should throw an NotFoundError', async () => {
+    const id = 3;
+
+    Topic.findByPk.mockImplementationOnce(() => null);
+
+    const error = () => CoursesController.getTopic(id);
+
+    expect(error).rejects.toThrow(Err.NotFoundError);
+  });
+});
+
+describe('getChapter', () => {
+  it('should return a topic', () => {
+    const id = 1;
+    const spy = jest.spyOn(Chapter, 'findByPk');
+    CoursesController.getChapter(id);
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith(id);
+  });
+
+  it('should throw an NotFoundError', async () => {
+    const id = 3;
+
+    Chapter.findByPk.mockImplementationOnce(() => null);
+
+    const error = () => CoursesController.getChapter(id);
+
+    expect(error).rejects.toThrow(Err.NotFoundError);
+  });
+});
+
+describe('getActivity', () => {
+  it('should return a topic', () => {
+    const id = 1;
+    const spy = jest.spyOn(Activity, 'findByPk');
+    CoursesController.getActivity(id);
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith(id);
+  });
+
+  it('should throw an NotFoundError', async () => {
+    const id = 3;
+
+    Activity.findByPk.mockImplementationOnce(() => null);
+
+    const error = () => CoursesController.getActivity(id);
+
+    expect(error).rejects.toThrow(Err.NotFoundError);
+  });
+});
+
+describe('activityDone', () => {
+  it('should create one activityDone data', () => {
+    const activityId = 1;
+    const userId = 1;
+    const done = true;
+
+    const spy = jest.spyOn(Activity, 'create');
+    CoursesController.activityDone(activityId, userId);
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith({ activityId, userId, done });
+  });
+});
+
+describe('getProgram', () => {
+  it('should return the program of the course', () => {
+    const courseId = 2;
+    const spy = jest.spyOn(Chapter, 'findAll');
+    CoursesController.getProgram(courseId);
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith(expect.objectContaining({ where: { courseId } }));
   });
 });
